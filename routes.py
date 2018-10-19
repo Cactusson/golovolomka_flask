74f452963cf538b9952c78392cfc59d5cc17ef2e
Cactusson <matt8th@gmail.com>
2018-10-19 10:00:25 +0300

    ваще добавлено

--- routes.py
+++ routes.py
@@ -6,3 +6,13 @@ from golovolomka import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/blur/')
+def blur():
+    return render_template('blur.html')
+
+
+@app.route('/battleships/')
+def battleships():
+    return render_template('battleships.html')
